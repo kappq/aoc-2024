b3b961355b3e6fb25e9f4f8db0ac165d7c610a40
Kappa <f.cappetti.05@gmail.com>
2024-12-07 19:23:22 +0100

    Day 07

---
+++ day07/part1.py
@@ -0,0 +1,28 @@
+equations = []
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        result, operands = line.split(": ")
+        equations.append((int(result), list(map(int, operands.split()))))
+
+
+def test_ops(result, curr, operands):
+    if not operands and result == curr:
+        return True
+    elif not operands:
+        return False
+
+    result_plus = test_ops(result, curr + operands[0], operands[1:])
+    result_mult = test_ops(result, curr * operands[0], operands[1:])
+
+    return result_plus or result_mult
+
+
+tot = 0
+
+for equation in equations:
+    result, operands = equation
+    if test_ops(result, operands[0], operands[1:]):
+        tot += result
+
+print(tot)
